6fe75cfbd94f91955393d37906508e69f1fa83e5
fujimurakengo <naruawaryousuke@gmail.com>
2017-09-23 14:30:59 +0900

    全部のボタンつかえるよ

---
+++ bottons/bottons.ino
@@ -0,0 +1,26 @@
+
+#define DAT 6
+#define DATA2   7
+#define CLK  8
+char botans[12][5]={'B','Y','SEL','STAR','up','down','lef','rig','A','X','L','R'};
+void setup(){  
+ 
+  pinMode(DAT,INPUT);
+  pinMode(CLK,OUTPUT);
+  pinMode(DATA2 ,OUTPUT);
+   Serial.begin(9600);
+}//20
+
+void loop(){
+  
+    digitalWrite(DATA2,HIGH);
+    digitalWrite(DATA2,LOW);
+
+   for(int clk=0;clk<12;clk++){
+     if(digitalRead(DAT) == LOW){
+      
+       Serial.println(botans[clk]);
+                             
+    }    
+   }
+}
